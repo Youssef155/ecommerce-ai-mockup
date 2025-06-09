359ceaf639598d0a365952b11e760e659196c9c7
Youssef George <yousifkhaleel00@gmail.com>
2025-06-10 01:16:21 +0300

    add infrastructure service registration

---
+++ ECommerceAIMockUp.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceAIMockUp.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            // logger service and email service
+
+            return services;
+        }
+    }
+}
