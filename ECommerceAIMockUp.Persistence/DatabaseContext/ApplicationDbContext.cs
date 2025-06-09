40627a329f3aac182bd1deebc55afba6932779e5
Youssef George <yousifkhaleel00@gmail.com>
2025-06-10 00:55:00 +0300

    add persistence registration service and dbContext

---
+++ ECommerceAIMockUp.Persistence/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using ECommerceAIMockUp.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Persistence.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in base.ChangeTracker.Entries<BaseEntity>()
+                .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+            {
+                item.Entity.UpdatedAt = DateTime.Now;
+
+                if (item.State == EntityState.Added)
+                {
+                    item.Entity.CreatedAt = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ ECommerceAIMockUp.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceAIMockUp.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("ApplicationConnectionString"));
+            });
+
+            return services;
+        }
+    }
+}
