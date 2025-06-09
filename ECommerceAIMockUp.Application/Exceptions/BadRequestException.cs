2cd686532ee205763266bb8ee13ee8e3ee5b54e8
Youssef George <yousifkhaleel00@gmail.com>
2025-06-10 01:04:24 +0300

    add NotFound and BadRequest exceptions

---
+++ ECommerceAIMockUp.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+
+        public BadRequestException(string message, ValidationResult validationResult) : base(message)
+        {
+            ValidatoinErrors = validationResult.ToDictionary();
+        }
+
+        public IDictionary<string, string[]> ValidatoinErrors { get; set; }
+    }
+}
