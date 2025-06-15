f8b19deaf00b3f705a341e972b241940b5f9e707
Omar <omarmehana15@gmail.com>
2025-06-16 02:40:31 +0300

    Add IBaseRepository interface and BaseRepository class

---
+++ ECommerceAIMockUp.Application/Contracts/Repositories/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.Contracts.Repositories
+{
+    public interface IBaseRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync(bool tracking, params Expression<Func<T, object>>[] includes);
+        Task<T> GetByIdAsunc(int id, bool tracking, params Expression<Func<T, object>>[] includes);
+        Task<T> CreateAsync(T entity);
+        T Update(T entity);
+        Task DeleteAsync(int id);
+    }
+}
