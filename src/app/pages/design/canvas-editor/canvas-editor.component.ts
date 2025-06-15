62c3eb0f339a858da4c4561969c6628a7c370851
neama gamal <126347630+neamagamal@users.noreply.github.com>
2025-06-15 15:14:30 +0300

    Initial commit with Frontend setup

---
+++ src/app/pages/design/canvas-editor/canvas-editor.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-editor',
+ standalone: true, 
+  imports: [CommonModule],
+  templateUrl: './canvas-editor.component.html',
+  styleUrl: './canvas-editor.component.css'
+})
+export class CanvasEditorComponent {
+
+}
