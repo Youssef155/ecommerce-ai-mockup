ce7d4aa1eaf9a0beb65f9899faac0c27589dbeba
Youssef George <yousifkhaleel00@gmail.com>
2025-06-15 15:56:07 +0300

    add AILog entity

---
+++ ECommerceAIMockUp.Domain/Entities/AILog.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceAIMockUp.Domain.Entities;
+
+public class AILog
+{
+    public int Id { get; set; }
+    public string RequestedAI { get; set; }
+    public string RequestType { get; set; }
+    public string PromptText { get; set; }
+
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
+}
