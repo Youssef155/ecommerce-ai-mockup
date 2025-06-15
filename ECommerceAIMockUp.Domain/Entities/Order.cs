8578ec02716adab48ae9a1f845b4e1b182b965cc
Youssef George <yousifkhaleel00@gmail.com>
2025-06-15 14:50:57 +0300

    add Order entity and Address as Value Object

---
+++ ECommerceAIMockUp.Domain/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceAIMockUp.Domain.Common;
+using ECommerceAIMockUp.Domain.ValueObjects;
+
+namespace ECommerceAIMockUp.Domain.Entities;
+
+public class Order : BaseEntity
+{
+    public string AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
+
+    public string Status { get; set; }
+
+    public string PaymentStatus { get; set; }
+    public DateTime? PaymentDate { get; set; }
+
+    public double OrderTotal { get; set; }
+    public DateTime? OrderDate { get; set; }
+    
+    public string? SessionId { get; set; }
+    public string? PaymentIntentId { get; set; }
+
+    public string PhoneNumber { get; set; }
+    public Address ShippingAddress { get; set; }
+
+}
---
+++ ECommerceAIMockUp.Domain/ValueObjects/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Domain.ValueObjects;
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Governorate { get; set; }
+    public string Zip { get; set; }
+}
