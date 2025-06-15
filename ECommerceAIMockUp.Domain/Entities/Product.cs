b7fd731996d507d2d19e71264d5ca73599eed2db
Youssef George <yousifkhaleel00@gmail.com>
2025-06-15 14:13:35 +0300

    add product entity

---
+++ ECommerceAIMockUp.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceAIMockUp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Gender { get; set; }
+    public string Season { get; set; }
+    public double Price { get; set; }
+}
