c3d54369437a66a0a684329900d665d25dc72c5a
Youssef George <yousifkhaleel00@gmail.com>
2025-06-16 20:11:08 +0300

    update AI Log entity

--- ECommerceAIMockUp.Domain/Entities/AILog.cs
+++ ECommerceAIMockUp.Domain/Entities/AILog.cs
@@ -4,6 +4,10 @@ public class AILog
 {
     public string RequestType { get; set; }
     public string PromptText { get; set; }
+    public bool IsSuccesed { get; set; }
+
+    public int DesignId { get; set; }
+    public Design Design { get; set; }
 
     public int AppUserId { get; set; }
     public AppUser AppUser { get; set; }
