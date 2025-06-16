161aba6a7945cac04d3980524c3f16879f5434c2
neama gamal <126347630+neamagamal@users.noreply.github.com>
2025-06-16 19:40:53 +0300

    add frontend layer

---
+++ ECommerceAIMockUp.Frontend/front-end/src/app/pages/design/design-preview/design-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design-preview',
+  imports: [],
+  templateUrl: './design-preview.component.html',
+  styleUrl: './design-preview.component.css'
+})
+export class DesignPreviewComponent {
+
+}
