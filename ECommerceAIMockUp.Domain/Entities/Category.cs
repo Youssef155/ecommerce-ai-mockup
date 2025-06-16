ff88fca032f2d6dc355dcff450d1f14e7e009ba7
Youssef George <yousifkhaleel00@gmail.com>
2025-06-16 20:08:13 +0300

    update category and product relation

--- ECommerceAIMockUp.Domain/Entities/Category.cs
+++ ECommerceAIMockUp.Domain/Entities/Category.cs
@@ -11,6 +11,4 @@ namespace ECommerceAIMockUp.Domain.Entities;
 public class Category : BaseEntity
 {
     public string Name { get; set; }
-    public int ProductId { get; set; }
-    public Product Product { get; set; }
 }
