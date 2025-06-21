138765e4e7c8713bdf90d6e2ff3d11ad342bf831
Omar <omarmehana15@gmail.com>
2025-06-21 15:07:21 +0300

    Add AppUserSeeder Class

--- ECommerceAIMockUp.Infrastructure/Configurations/AppUserConfiguration.cs
+++ ECommerceAIMockUp.Infrastructure/Configurations/AppUserConfiguration.cs
@@ -1,14 +1,34 @@
 ﻿using ECommerceAIMockUp.Domain;
+using ECommerceAIMockUp.Domain.ValueObjects;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ECommerceAIMockUp.Infrastructure.Configurations
 {
-    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    public static class AppUserSeeder
     {
-        public void Configure(EntityTypeBuilder<AppUser> builder)
+        public static async Task SeedAppUserAsync(UserManager<IdentityUser> userManager)
         {
+            var user = new AppUser
+            {
+                UserName = "omar@gmail.com",
+                Email = "omar@gmail.com",
+                EmailConfirmed = true,
+                FirstName = "Omar",
+                LastName = "Mehanna",
+                Address = new Address() { Governorate = "Alexadria", City = "Camp shezar", Street = "Hehya"}
+            };
 
+            var result = await userManager.CreateAsync(user, "Omar@123");
+            if (!result.Succeeded)
+            {
+                throw new Exception("Failed to create admin user: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+            }
+
+            await userManager.AddToRoleAsync(user, "Customer");
+            
         }
     }
 }
+
