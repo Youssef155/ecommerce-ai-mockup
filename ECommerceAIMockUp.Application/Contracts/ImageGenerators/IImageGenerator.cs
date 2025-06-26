0d2e1eceed90a444574cca88028015e397b7283f
Omar <omarmehana15@gmail.com>
2025-06-26 22:37:24 +0300

    Add IImageGenerator interface

---
+++ ECommerceAIMockUp.Application/Contracts/ImageGenerators/IImageGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.Contracts.ImageGenerators
+{
+    public interface IImageGenerator
+    {
+        Task<string> ImageGenerator(string prompt);
+    }
+}
