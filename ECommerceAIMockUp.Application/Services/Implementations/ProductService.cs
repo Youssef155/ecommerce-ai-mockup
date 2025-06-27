357529ce5bc0c485547ab1a5c77a77b6b3e8b26a
Youssef George <yousifkhaleel00@gmail.com>
2025-06-27 17:01:55 +0300

    add product service's interface & implementation

---
+++ ECommerceAIMockUp.Application/Services/Implementations/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using ECommerceAIMockUp.Application.Contracts.Repositories;
+using ECommerceAIMockUp.Application.DTOs.Product;
+using ECommerceAIMockUp.Application.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.Services.Implementations;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<ProductBasicDto> GetProductBasicAsync(int productId)
+    {
+        var product = await _productRepository.GetByIdWithVariantsAsync(productId);
+
+        if (product == null)
+            throw new KeyNotFoundException("Product not found.");
+
+        var sizes = product.ProductDetails
+            .Select(v => v.Size)
+            .Distinct()
+            .OrderBy(s => s)
+            .ToList();
+
+        return new ProductBasicDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Description = product.Description,
+            AvailableSizes = sizes
+        };
+    }
+
+    public async Task<ColorOptionsDto> GetAvailableColorsAsync(int productId, string size)
+    {
+        var product = await _productRepository.GetByIdWithVariantsAsync(productId);
+
+        if (product == null)
+            throw new KeyNotFoundException("Product not found.");
+
+        var colors = product.ProductDetails
+            .Where(v => v.Size.Equals(size, StringComparison.OrdinalIgnoreCase))
+            .Select(v => v.Color)
+            .Distinct()
+            .OrderBy(c => c)
+            .ToList();
+
+        if (!colors.Any())
+            throw new InvalidOperationException("No colors found for the specified size.");
+
+        return new ColorOptionsDto
+        {
+            Size = size,
+            AvailableColors = colors
+        };
+    }
+}
+
---
+++ ECommerceAIMockUp.Application/Services/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceAIMockUp.Application.DTOs.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductBasicDto> GetProductBasicAsync(int productId);
+        Task<ColorOptionsDto> GetAvailableColorsAsync(int productId, string size);
+    }
+
+}
