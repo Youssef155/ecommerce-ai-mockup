8f83f2610bc55e4d4c096594203626e1b557737d
Youssef George <yousifkhaleel00@gmail.com>
2025-06-27 16:20:57 +0300

    add basic product dto

---
+++ ECommerceAIMockUp.Application/DTOs/Product/ProductBasicDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceAIMockUp.Application.DTOs.Product;
+
+public class ProductBasicDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> AvailableSizes { get; set; }
+}
