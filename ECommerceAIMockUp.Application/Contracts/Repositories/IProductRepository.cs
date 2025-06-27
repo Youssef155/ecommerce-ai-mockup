291cb9927c89c906ad2595f21d55a6eea6099adc
Youssef George <yousifkhaleel00@gmail.com>
2025-06-27 16:50:13 +0300

    add product repository

---
+++ ECommerceAIMockUp.Application/Contracts/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceAIMockUp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.Contracts.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdWithVariantsAsync(int productId);
+    }
+}
---
+++ ECommerceAIMockUp.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceAIMockUp.Application.Contracts.Repositories;
+using ECommerceAIMockUp.Domain.Entities;
+using ECommerceAIMockUp.Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAIMockUp.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product?> GetByIdWithVariantsAsync(int productId)
+        {
+            return await _context.Products
+                .Include(p => p.ProductDetails)
+                .FirstOrDefaultAsync(p => p.Id == productId);
+        }
+
+    }
+}
