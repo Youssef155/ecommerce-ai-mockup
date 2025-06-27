38ebb3ece4af964ee7d7312bb18794fbd88450f1
Youssef George <yousifkhaleel00@gmail.com>
2025-06-27 18:17:58 +0300

    add price to ProductBasicDto

--- ECommerceAIMockUp.Application/DTOs/Product/ProductBasicDto.cs
+++ ECommerceAIMockUp.Application/DTOs/Product/ProductBasicDto.cs
@@ -5,5 +5,6 @@ public class ProductBasicDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public double Price { get; set; }
     public List<string> AvailableSizes { get; set; }
 }
