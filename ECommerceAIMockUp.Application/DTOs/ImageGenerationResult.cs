43f168437093381aa3a0b9c67a186c78fa3022ee
Omar <omarmehana15@gmail.com>
2025-06-29 17:09:19 +0300

    Add ImageGenerationResult class

---
+++ ECommerceAIMockUp.Application/DTOs/ImageGenerationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAIMockUp.Application.DTOs
+{
+    public class ImageGenerationResult
+    {
+        public string ImageUrl { get; set; }
+        public byte[] ImageBytes { get; set; }
+    }
+}
