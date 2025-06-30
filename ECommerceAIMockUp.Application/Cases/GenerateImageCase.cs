18f0bf64e423d9b3300235e4dfa8da75087bfe35
Omar <omarmehana15@gmail.com>
2025-06-30 03:57:49 +0300

    Update GenerateImageCase to save the prompt to AILog table in database

--- ECommerceAIMockUp.Application/Cases/GenerateImageCase.cs
+++ ECommerceAIMockUp.Application/Cases/GenerateImageCase.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECommerceAIMockUp.Application.Contracts.ImageGenerators;
+using ECommerceAIMockUp.Application.Contracts.Repositories;
 using ECommerceAIMockUp.Application.DTOs;
 using ECommerceAIMockUp.Application.Wrappers;
 using ECommerceAIMockUp.Domain.Entities;
@@ -14,10 +15,12 @@ namespace ECommerceAIMockUp.Application.Cases
     {
         private readonly IPromptValidator _promptValidator;
         private readonly IImageGenerator _imageGenerator;
-        public GenerateImageCase(IPromptValidator promptValidator, IImageGenerator imageGenerator)
+        private readonly IBaseRepository<AILog> _logRepository;
+        public GenerateImageCase(IPromptValidator promptValidator, IImageGenerator imageGenerator, IBaseRepository<AILog> logRepository)
         {
             _promptValidator = promptValidator;
             _imageGenerator = imageGenerator;
+            _logRepository = logRepository;
         }
 
         public async Task<Response<Image>> GenerateImageAsync(string prompt, string userId)
@@ -28,13 +31,15 @@ namespace ECommerceAIMockUp.Application.Cases
             if (!promptValidationResult.IsValid)
             {
                 aILog.IsSuccesed = false;
-                //Save the aiLog to the database
+                await _logRepository.CreateAsync(aILog);
+                await _logRepository.SaveChangesAsync();
                 return new Response<Image> { IsSucceeded = false, Error = promptValidationResult.Error };
             }
             Image image = await _imageGenerator.GenerateImageAsync(prompt);
             aILog.IsSuccesed = true;
             //Add reference to the design in aiLog
-            //Save the aiLog to the database
+            await _logRepository.CreateAsync(aILog);
+            await _logRepository.SaveChangesAsync();
             return new Response<Image> { Data = image, IsSucceeded = true };
             
         }
